59ecea25f770515cc56cc27709741b6f3f274ece
haroun <haroun.khenfri@powercoders.org>
2024-05-05 13:30:22 +0200

    Remove unnecessary files from the folder

--- week4/tuesday/exercice-mult2/script.js
+++
@@ -1,3 +0,0 @@
-for (let i = 0; i <= 10; i++) {
-  console.log(`${i} * 9 = ${i * 9}`);
-}
--- week4/tuesday/exercice-multiplication/script.js
+++
@@ -1,6 +0,0 @@
-let input = prompt("Type a number between 0 and 10");
-if (input >= 0 && input <= 20) {
-  alert(`${input} * 9 = ${input * 9}`);
-} else {
-  alert(`Your number is wrong`);
-}
